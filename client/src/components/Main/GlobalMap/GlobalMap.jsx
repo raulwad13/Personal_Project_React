a0c8676ae87c99003a917fd3d75ba847a43959ac
raulwad13 <rawad13@myyahoo.com>
2024-01-30 18:45:16 +0100

    start map

--- client/src/components/Main/GlobalMap/GlobalMap.jsx
+++ client/src/components/Main/GlobalMap/GlobalMap.jsx
@@ -1,17 +1,29 @@
 import React, { useEffect, useState, useRef } from "react";
 import "./GlobalMap.css";
+import { MapContainer,  useMap } from 'react-leaflet'
 
 
 
 const GlobalMap = () => {
+  useEffect(() => {
+    const map = mapRef.current.leafletElement;
+    const bounds = [[-26.5, -25], [1021.5, 1023]];
+    const image = L.imageOverlay("https://i.imgur.com/Ion6X7C.jpg", bounds).addTo(
+      map
+    );
+    map.fitBounds(image.getBounds());
+  }, []);
   return (
     <div className="map-container">
 
-      <img
-        src= "../../../../public/assets/maps/mapaGlobal.png"
-        alt="Map"
-        style={{ width: "100%", height: "100%", objectFit: "cover" }}
-      />
+<MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+    <Marker position={position}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>,
+
     </div>
     
   );
